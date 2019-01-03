9516d9d1711dfac17304a30646e87c182e9b04c2
sireesha_k <sireesha9.k@gmail.com>
2019-01-03 12:58:25 +0530

    updated

---
+++ src/app/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  
+    constructor() { }
+    getHeroes(){
+return HEROES;
+    }
+}
